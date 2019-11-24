48a5af2cddeb92cc7aee6cee9d4801ba1db677c5
userr23 <aavvsskr@gmail.com>
2019-11-24 20:26:19 +0200

    some updates

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,7 +1,6 @@
 export toFixed                           from './utilities/toFixed';
 export getRandomArbitrary                from './utilities/getRandomArbitrary';
 export getRandomIntInclusive             from './utilities/getRandomIntInclusive';
-export getRGBA                           from './utilities/getRGBA';
 export { encode, decode }                from './utilities/encoder';
 
 export const log10  = Math.log10;
