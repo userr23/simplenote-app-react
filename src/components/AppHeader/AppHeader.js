f7ef42eb778b19c9ee3445c3ba0f0ba8f4134734
userr23 <aavvsskr@gmail.com>
2019-12-10 22:48:40 +0200

    AppHeader refactoring; style changes

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,9 +1,17 @@
 import React               from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled              from 'styled-components';
+import PropTypes           from 'prop-types';
+
+const Wrapper = styled.div`
+    flex-wrap: wrap;
+    align-items: flex-end;
+    margin-top: 1rem;
+`;
 
 export default function AppHeader ( { print, omit, total } ) {
     return (
-        <div className="app-header d-flex">
+        <Wrapper className="app-header d-flex">
             <h1>
                 SimpleNote
                 <sup>
@@ -12,6 +20,12 @@ export default function AppHeader ( { print, omit, total } ) {
             </h1>
             {( omit !== 0 ) && <h5>{print} available to print, {omit} omitted</h5>}
             {( omit === 0 ) && <h5>All {total} notes available to print</h5>}
-        </div>
+        </Wrapper>
     );
 }
+
+AppHeader.propTypes = {
+    print: PropTypes.number,
+    omit : PropTypes.number,
+    total: PropTypes.number
+};
